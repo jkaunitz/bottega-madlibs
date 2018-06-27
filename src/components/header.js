a3a6a587f11fa469fb033a3ea10881dd31bef344
jkaunitz <justin.kaunitz@gmail.com>
2018-06-27 14:07:03 -0700

    created background header skews

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,8 @@ import React from "react";
 const Header = () => {
   return (
     <div className="header">
+      <div className="header__skew" />
+      <div className="header__border" />
       <h1>Bottega Madlibs</h1>
       <p>
         Fill out the fields below and click the generate button to see the Mad
